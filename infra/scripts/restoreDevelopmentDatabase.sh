7cb9066ce3a191a95eec777840f8fbcceec6ac1e
NikolaosKantartzopoulos <nkantartzopoulos@gmail.com>
2024-03-01 15:47:41 +0200

    Add relative db paths

--- infra/scripts/restoreDevelopmentDatabase.sh
+++ infra/scripts/restoreDevelopmentDatabase.sh
@@ -7,4 +7,4 @@ docker exec -i mysql-dev mysql -uroot -prpwd -e "drop database campaign_journal;
 docker exec -i mysql-dev mysql -uroot -prpwd -e "create database campaign_journal;" 
 
 
-docker exec -i mysql-dev mysql -uroot -prpwd campaign_journal --force < /home/nik/code/databases/mysql/backups/$inputDate.sql
+docker exec -i mysql-dev mysql -uroot -prpwd campaign_journal --force < ../../../campaign-journal-backend/mysql/backups/$inputDate.sql
